0c87d569c149c50cd676ac7e55d9f5fd04929861
alextroupi <alextroupi@hotmail.gr>
2025-02-20 19:29:14 +0200

    optimized grid function to keep each cell once, added a function that count the total positions in each cell

--- src/mt/density/export_density_maps.py
+++ src/mt/density/export_density_maps.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     
     config_file = open(sys.argv[1], "r",encoding='utf-8')
     config = json.load(config_file)
-
+    print("Alex alex")
 
 
     #print("Creating density maps for " + out_name)
